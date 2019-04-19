32c79d6358fbb5ece9469dcecf703d694f265e0a
jmagsalin <jmagsalin@hotmail.com>
2019-04-19 16:15:43 -0700

    answer 7

--- js-intro.js
+++ js-intro.js
@@ -2,23 +2,23 @@
 
 // Consider this variable:
 
-var mantra = "Be the dev"
-// // console.log(mantra[0]);
-// // 1. Write the code that returns 'B' from mantra.
+// var mantra = "Be the dev"
+// // // console.log(mantra[0]);
+// // // 1. Write the code that returns 'B' from mantra.
+// //
+// // if (mantra.includes("x")){
+// //   console.log("mantra includes x");
+// // } else {console.log("mantra does not include x");}
 //
-// if (mantra.includes("x")){
-//   console.log("mantra includes x");
-// } else {console.log("mantra does not include x");}
-
-// function letterId(words)for (let i=0; i < words.length; i++) {
-//   if (words) = "x"
+// // function letterId(words)for (let i=0; i < words.length; i++) {
+// //   if (words) = "x"
+// // }
+// // 3. Write the code that determines if there is a 'v' in mantra.
+// if (mantra.includes("v")){
+//   console.log("mantra includes v");
+// } else {console.log("mantra does not include v");
 // }
-// 3. Write the code that determines if there is a 'v' in mantra.
-if (mantra.includes("v")){
-  console.log("mantra includes v");
-} else {console.log("mantra does not include v");
-}
-// Stretch: Write the code that returns the position of 'v' in mantra.
+// // Stretch: Write the code that returns the position of 'v' in mantra.
 
 
 // Consider the following statement:
@@ -48,9 +48,16 @@ if (mantra.includes("v")){
 // 5. Update the imLost and imFrustated variables above the statement to the values required to log "Keep coding!"
 
 // 6a. Recall the mantra variable from the first question. Write the code that logs each letter of mantra using a for loop.
-
-// Stretch: Using a while loop.
-
+// var mantra = "Be the dev"
+// for (let i=0; i < mantra.length; i++)  {
+// //   console.log(mantra[i]);
+// }
+// // Stretch: Using a while loop.
+// var mantra = "Be the dev"
+// var i = 0
+// var text = ""
+// while (mantra[i]){text += mantra[i] + "<br>"; i++}
+//   console.log(mantra[i]);
 // Super Stretch: Using forEach().
 
 // Consider the following variable:
@@ -58,13 +65,27 @@ if (mantra.includes("v")){
 var message = "thisisateststring"
 
 // 7. Write a function that takes a string like message as an argument and returns the string without vowels.
+function noVowels(words){
+  var newArr = []
+  var letters = words.split("")
+  for (let i = 0; i < letters.length; i++) {
+    if (letters[i] !== "a" && letters[i] !== "e" && letters[i] !== "i" && letters[i] !== "o" && letters[i] !== "u") {
+      newArr.push(letters[i])
+    }var sentence = newArr.join("")
+
+    }return sentence
+  }
+
+  console.log(noVowels(message));
+
+
 
 // Stretch: Update your function to throw an exception if the argument is not a string.
 
 // Super Duper Stretch: Update your function to throw a TypeError if the argument is not a string.
 
 // Consider the following variable:
-
-var toonimals = [ {name: 'Itchy', animal: 'mouse'}, {name: 'Stimpy', animal: 'cat'}, {name: 'Daffy', animal: 'duck'}, {name: 'Scratchy', animal: 'cat'}, {name: 'Ren', animal: 'dog'}, {name: 'Felix', animal: 'cat'}]
+//
+// var toonimals = [ {name: 'Itchy', animal: 'mouse'}, {name: 'Stimpy', animal: 'cat'}, {name: 'Daffy', animal: 'duck'}, {name: 'Scratchy', animal: 'cat'}, {name: 'Ren', animal: 'dog'}, {name: 'Felix', animal: 'cat'}]
 
 // 8. Write a function that takes an array like toonimals and returns an array with only the toons that are cats.
