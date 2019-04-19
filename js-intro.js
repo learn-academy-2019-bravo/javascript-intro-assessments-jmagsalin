d7a37a63b766a08b2f84072795f7d0a94ffd286d
jmagsalin <jmagsalin@hotmail.com>
2019-04-19 15:29:08 -0700

    Up to q4

--- js-intro.js
+++ js-intro.js
@@ -2,40 +2,48 @@
 
 // Consider this variable:
 
-var mantra = "Be the dev";
-This is a random change
-// 1. Write the code that returns 'B' from mantra.
+var mantra = "Be the dev"
+// // console.log(mantra[0]);
+// // 1. Write the code that returns 'B' from mantra.
+//
+// if (mantra.includes("x")){
+//   console.log("mantra includes x");
+// } else {console.log("mantra does not include x");}
 
-// 2. Write the code that determines if there is a 'x' in mantra.
-
+// function letterId(words)for (let i=0; i < words.length; i++) {
+//   if (words) = "x"
+// }
 // 3. Write the code that determines if there is a 'v' in mantra.
-
+if (mantra.includes("v")){
+  console.log("mantra includes v");
+} else {console.log("mantra does not include v");
+}
 // Stretch: Write the code that returns the position of 'v' in mantra.
 
 
 // Consider the following statement:
 
-var codingIsEasy
-var imBored
-
-if (codingIsEasy && imBored) {
-    console.log("I need more!")
-} else {
-    console.log("Steady as she goes")
-}
+// var codingIsEasy = true
+// var imBored = true
+//
+// if (codingIsEasy && imBored) {
+//     console.log("I need more!")
+// } else {
+//     console.log("Steady as she goes")
+// }
 
 // 4. Update the codingIsEasy and imBored variables above the statement to the values required to log "I need more!"
 
 // Consider the following statement:
 
-var imLost
-var imFrustrated
-
-if (imLost || imFrustrated) {
-    console.log('Break time!')
-} else {
-    console.log('Keep coding!')
-}
+// var imLost = false
+// var imFrustrated = false
+//
+// if (imLost || imFrustrated) {
+//     console.log('Break time!')
+// } else {
+//     console.log('Keep coding!')
+// }
 
 // 5. Update the imLost and imFrustated variables above the statement to the values required to log "Keep coding!"
 
